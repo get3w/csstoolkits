008d2edd7a0f43b5e68f72d031001404c59c6107
joozo <corp@joozo.cn>
2015-08-24 16:02:50 +0800

    *.less to *.scss

--- gulp/css.js
+++ gulp/css.js
@@ -4,14 +4,14 @@ var concat = require('gulp-concat');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
 var cssmin = require('gulp-cssmin');
-var less = require('gulp-less');
+var sass = require('gulp-sass');
 var combiner = require('stream-combiner2');
 
 gulp.task('css', function () {
 
     var combined = combiner.obj([
-        gulp.src(['./less/csstoolkits.less']),
-        less(),
+        gulp.src(['./scss/csstoolkits.scss']),
+        sass(),
         concat('ct.css'),
         gulp.dest('./dist/css')
     ]);
