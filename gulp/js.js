35093136075f1c18c53e7ab35735f53772d155c5
joozo <corp@joozo.com>
2015-11-24 11:06:03 +0800

    edit gulp files

--- gulp/js.js
+++ gulp/js.js
@@ -4,7 +4,7 @@ var concat = require('gulp-concat');
 var jshint = require('gulp-jshint');
 
 gulp.task('js', function() {
-    return gulp.src('./ctjs/*.js')
-        .pipe(concat('ctjs.js'))
-        .pipe(gulp.dest('js'))
+    return gulp.src('./js/*.js')
+        .pipe(concat('js.js'))
+        .pipe(gulp.dest('dist/js'))
 });
